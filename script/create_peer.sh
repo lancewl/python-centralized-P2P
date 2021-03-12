eb2caea31ef65e4c512068845767e168b464fe7e
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-03-12 12:29:47 -0600

    Update create_peer

--- script/create_peer.sh
+++ script/create_peer.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    echo "Usage: create_peer.sh [Peer Count] [File Count] [File Size]"
+fi
+
+N=$1
+F=$2
+S=$3
+
 rm -rf peer_folder*
 
-for i in {1..8}
+for (( i=1; i<=$N; i++ ))
 do
     mkdir "peer_folder$i"
     cd "peer_folder$i"
-    for j in {1..10}
+    for (( j=1; j<=$F; j++ ))
     do
-        mkfile 128 "data$j.txt"
+        mkfile $S "data$j.txt"
     done
     cd ..
 done
